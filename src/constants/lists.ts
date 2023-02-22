7a177086cb0097629ccd920ddc88db41fa8fd727
tenda0208 <444717448@qq.com>
2023-02-22 21:13:41 +0800

    https://github.com/tenda0208/SecretSwap.git

---
+++ src/constants/lists.ts
@@ -0,0 +1,5 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://bafybeiekwsfcx4fgyyo37fw6jdrglgd6mwn2vcatz464rt6dt66b35qudm.ipfs.w3s.link/Secret.tokenlist.json'
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
